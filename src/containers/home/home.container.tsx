031686debc4cb58b1b00222b89905921faab7dff
Herman Leus <hermanleus@Hermans-MacBook-Pro.local>
2020-07-04 14:46:47 +0300

    Added homepage heading

--- src/containers/home/home.container.tsx
+++ src/containers/home/home.container.tsx
@@ -1,10 +1,33 @@
+import "./home.scss";
+
 import React, { FunctionComponent } from "react";
 
+import Container from "layouts/container";
 import Default from "layouts/default.layout";
 
 const HomeContainer: FunctionComponent = () => (
   <Default>
-    <div className="home-module"></div>
+    <div className="home-module">
+      <Container>
+        <header className="main-module-header">
+          <h1 className="heading">
+            PhutureDAO provides a range of benchmark INDEXES for Crypto
+          </h1>
+          <button className="button">All Indexes</button>
+        </header>
+
+        <section className="top-index-section">
+          <header>
+            <h2 className="heading">Top Index</h2>
+            <p>
+              The DeFi Index - Top 5 (Market Cap) <br />
+              Index price <b>$78 or 0.34 ETH</b> / % Change 24h total index
+              <b>+55</b>
+            </p>
+          </header>
+        </section>
+      </Container>
+    </div>
   </Default>
 );
 
