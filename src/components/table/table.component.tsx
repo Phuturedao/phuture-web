82bbf0d98861c63663064ccaceccd5232b7dcd6d
Herman Leus <hermanleus@Hermans-MacBook-Pro.local>
2020-07-10 01:41:38 +0300

    Assets Table

--- src/components/table/table.component.tsx
+++ src/components/table/table.component.tsx
@@ -7,15 +7,82 @@ import PhutureLogo from "assets/svgs/phuture-logo";
 
 interface Props extends RouteComponentProps<any> {
   title?: string;
+  type?: string;
 }
 
+const INDEX_HEADERS = ["Name", "Market size 24h", "%loss/24h", "%gain/24h"];
+const ASSET_HEADERS = [
+  "Name",
+  "USD Amount",
+  "ETH Amount",
+  "PHTR Amount",
+  "%gain/24h",
+  "Actions",
+];
+
+const IndexRow: FunctionComponent<any> = ({ history, width }) => (
+  <tr
+    onClick={() => {
+      history.push(`/indexes/defi`);
+    }}
+  >
+    <td className="logo">
+      <div>
+        <PhutureLogo /> <span>Company Name</span>
+      </div>
+    </td>
+    <td>10%</td>
+    <td>5%</td>
+    <td className="highlight">+15%</td>
+  </tr>
+);
+
+const colors: Array<string> = ["#0092CC", "#33C5FF", "#99E2FF"];
+
+const AssetRow: FunctionComponent<any> = ({ history, colorIndex }) => (
+  <tr
+    onClick={() => {
+      history.push(`/indexes/defi`);
+    }}
+  >
+    <td className="logo">
+      <div>
+        <i className="dot" style={{ background: colors[colorIndex] }} />
+        <span>Asset</span>
+      </div>
+    </td>
+    <td>$214</td>
+    <td>0.04 ETH</td>
+    <td>100 PHTR</td>
+    <td>5%</td>
+    <td>
+      <div className="actions">
+        <button className="button">Deposit</button>
+        <button className="button">Withdraw</button>
+      </div>
+    </td>
+  </tr>
+);
+
 //TODO in case of more complicated requirements change table to div based table
-const TableComponent: FunctionComponent<Props> = ({ title, history }) => {
-  //TODO calculate based on amount of columns
-  const width: string = `25%`;
+const TableComponent: FunctionComponent<Props> = ({
+  type = "index",
+  title,
+  history,
+}) => {
+  let headings: Array<string>;
+
+  switch (type) {
+    case "assets":
+      headings = ASSET_HEADERS;
+
+      break;
+    default:
+      headings = INDEX_HEADERS;
+  }
 
   return (
-    <div className="table-component">
+    <div className={`table-component ${type ?? ""}`}>
       {title && (
         <header>
           <h3 className="heading">{title}</h3>
@@ -24,32 +91,21 @@ const TableComponent: FunctionComponent<Props> = ({ title, history }) => {
       <table>
         <thead>
           <tr>
-            <td style={{ width }}>Name</td>
-            <td style={{ width }}>Market size 24h</td>
-            <td style={{ width }}>%loss/24h</td>
-            <td style={{ width }}>%gain/24h</td>
+            {headings.map((th, i) => (
+              <td key={i}>{th}</td>
+            ))}
           </tr>
         </thead>
         <tbody>
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => (
-            <tr
-              key={i}
-              onClick={() => {
-                history.push(`/indexes/defi`);
-              }}
-            >
-              <td style={{ width }} className="logo">
-                <div>
-                  <PhutureLogo /> <span>Company Name</span>
-                </div>
-              </td>
-              <td style={{ width }}>10%</td>
-              <td style={{ width }}>5%</td>
-              <td style={{ width }} className="highlight">
-                +15%
-              </td>
-            </tr>
-          ))}
+          {type === "index" &&
+            [1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => (
+              <IndexRow history={history} key={i} />
+            ))}
+
+          {type === "assets" &&
+            [0, 1, 2].map((i) => (
+              <AssetRow history={history} colorIndex={i} key={i} />
+            ))}
         </tbody>
       </table>
     </div>
--- src/containers/account/account.container.tsx
+++ src/containers/account/account.container.tsx
@@ -5,12 +5,14 @@ import React, { FunctionComponent } from "react";
 import Container from "layouts/container";
 import Default from "layouts/default.layout";
 import Deposit from "components/sections/deposit/deposit.component";
+import Table from "components/table/table.component";
 
 const AccountContainer: FunctionComponent = () => (
   <Default navigation={{ account: "0x9E39...6D94", index: "DeFi" }}>
     <div className="account-module">
       <Container>
         <Deposit balance="100 ETH" />
+        <Table type="assets" />
       </Container>
     </div>
   </Default>
