b2d43026775c6f38f349e90222c64b77f8a7e1da
Herman Leus <hermanleus@Hermans-MacBook-Pro.local>
2020-07-11 01:58:46 +0300

    Donut Chart

---
+++ src/components/charts/donut-chart/donut-chart.component.tsx
@@ -0,0 +1,70 @@
+import "./donut-chart.scss";
+
+import * as d3 from "d3";
+
+import React, { FunctionComponent, useEffect, useRef } from "react";
+
+const DonutChart: FunctionComponent = () => {
+  const chartRef: any = useRef(null);
+
+  useEffect(() => {
+    let width: number = 80;
+    let height: number = 80;
+    let margin: number = 1;
+
+    // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+    let radius: number = Math.min(width, height) / 2 - margin;
+    // append the svg object to the div called 'my_dataviz'
+    const svg: any = d3
+      .select(chartRef.current)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    // Create dummy data
+    const data: any = { a: 6, b: 3, c: 3 };
+
+    // set the color scale
+    const color = d3
+      .scaleOrdinal()
+      .domain(data)
+      .range(["#0092CC", "#33C5FF", "#99E2FF"]);
+
+    // Compute the position of each group on the pie:
+    const pie = d3.pie().value((d: any) => {
+      return d.value;
+    }) as any;
+
+    const data_ready = pie(d3.entries(data));
+
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    svg
+      .selectAll("whatever")
+      .data(data_ready)
+      .enter()
+      .append("path")
+      .attr(
+        "d",
+        d3
+          .arc()
+          .innerRadius(radius - 10) // This is the size of the donut hole
+          .outerRadius(radius)
+      )
+      .attr("fill", (d: any) => {
+        return color(d.data.key);
+      })
+      .attr("stroke", "white")
+      .style("stroke-width", "2px")
+      .style("opacity", 1);
+  }, []);
+
+  return (
+    <div className="donut-chart">
+      <div ref={chartRef}></div>
+    </div>
+  );
+};
+
+export default DonutChart;
--- src/components/sections/asset-stats/asset-stats.component.tsx
+++ src/components/sections/asset-stats/asset-stats.component.tsx
@@ -2,7 +2,10 @@ import "./asset-stats.scss";
 
 import React, { FunctionComponent } from "react";
 
+import DonutChart from "components/charts/donut-chart/donut-chart.component";
+import ETH from "assets/svgs/eth";
 import LineChart from "components/sections/line-chart-section/line-chart-section.component";
+import PTHR from "assets/imgs/phtr.png";
 
 export const PriceStats: FunctionComponent = () => (
   <section className="price-stats">
@@ -10,10 +13,30 @@ export const PriceStats: FunctionComponent = () => (
       <h5 className="heading">Title</h5>
       <div className="stats">
         <div>$100</div>
-        <div>0.4 ETH</div>
-        <div>1.00 PHTR</div>
+        <div>
+          <ETH /> 0.4 ETH
+        </div>
+        <div>
+          <img src={PTHR} alt="phuture dao logo" /> 1.00 PHTR
+        </div>
       </div>
     </header>
+  </section>
+);
+
+export const ProfitLoss: FunctionComponent = () => (
+  <section className="profit-loss">
+    <div className="chart">
+      <DonutChart />
+    </div>
+    <div>
+      <div className="title">24h Gain/Loss</div>
+      <div className="value down">-24%</div>
+    </div>
+    <div>
+      <div className="title">24h Gain/Loss</div>
+      <div className="value up">+24%</div>
+    </div>
   </section>
 );
 
@@ -21,6 +44,7 @@ const AssetStats: FunctionComponent = () => (
   <section className="asset-stats">
     <div>
       <PriceStats />
+      <ProfitLoss />
     </div>
     <div>
       <LineChart />
