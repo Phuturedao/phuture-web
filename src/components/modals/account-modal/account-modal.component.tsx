2aca3eb95e67f85b72c48eeca33238b6aae0c365
Herman Leus <hermanleus@Hermans-MacBook-Pro.local>
2020-07-12 23:28:57 +0300

    Account modal improvements

--- src/components/modals/account-modal/account-modal.component.tsx
+++ src/components/modals/account-modal/account-modal.component.tsx
@@ -1,28 +1,42 @@
 import "./account-modal.scss";
 
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 
 import DefaultModal from "components/modals/modal/modal.componnet";
 import copy from "copy-to-clipboard";
 
-const AccountModal: FunctionComponent<any> = (props) => (
-  <DefaultModal title="Account" className="account-modal" {...props}>
-    <div className="label">Connected with MetaMask</div>
-    <div>
-      <div className="account-hash">0x9E39...6D94</div>
-      <div className="actions">
-        <button
-          className="button alternative small"
-          onClick={() => {
-            copy("0x9E39...6D94");
-          }}
-        >
-          Copy
-        </button>
-        <button className="button small">Change</button>
+const AccountModal: FunctionComponent<any> = (props) => {
+  const [isCopied, setIsCopied] = useState(false);
+  const onClose = () => {
+    props.onClose();
+    setIsCopied(false);
+  };
+
+  return (
+    <DefaultModal
+      title="Account"
+      className="account-modal"
+      {...props}
+      onClose={onClose}
+    >
+      <div className="label">Connected with MetaMask</div>
+      <div>
+        <div className="account-hash">0x9E39...6D94</div>
+        <div className="actions">
+          <button
+            className={`button alternative small ${isCopied ? "success" : ""}`}
+            onClick={() => {
+              copy("0x9E39...6D94");
+              setIsCopied(true);
+            }}
+          >
+            {isCopied ? "Copied" : "Copy"}
+          </button>
+          <button className="button small">Change</button>
+        </div>
       </div>
-    </div>
-  </DefaultModal>
-);
+    </DefaultModal>
+  );
+};
 
 export default AccountModal;
