9b2f3fc8f740d49aa616f3959bf8c779926e9be7
Herman Leus <hermanleus@Hermans-MacBook-Pro.local>
2020-07-14 00:09:43 +0300

    Wallet modal

--- src/components/modals/account-modal/account-modal.component.tsx
+++ src/components/modals/account-modal/account-modal.component.tsx
@@ -3,9 +3,11 @@ import "./account-modal.scss";
 import React, { FunctionComponent, useState } from "react";
 
 import DefaultModal from "components/modals/modal/modal.componnet";
+import WalletModal from "components/modals/wallet-modal/wallet-modal.component";
 import copy from "copy-to-clipboard";
 
 const AccountModal: FunctionComponent<any> = (props) => {
+  const [isOpenWalletModal, setIsOpenWalletModal] = useState(false);
   const [isCopied, setIsCopied] = useState(false);
   const onClose = () => {
     props.onClose();
@@ -13,29 +15,47 @@ const AccountModal: FunctionComponent<any> = (props) => {
   };
 
   return (
-    <DefaultModal
-      title="Account"
-      className="account-modal"
-      {...props}
-      onClose={onClose}
-    >
-      <div className="label">Connected with MetaMask</div>
-      <div>
-        <div className="account-hash">0x9E39...6D94</div>
-        <div className="actions">
-          <button
-            className={`button alternative small ${isCopied ? "success" : ""}`}
-            onClick={() => {
-              copy("0x9E39...6D94");
-              setIsCopied(true);
-            }}
-          >
-            {isCopied ? "Copied" : "Copy"}
-          </button>
-          <button className="button small">Change</button>
+    <>
+      <DefaultModal
+        title="Account"
+        className="account-modal"
+        {...props}
+        onClose={onClose}
+      >
+        <div className="label">Connected with MetaMask</div>
+        <div>
+          <div className="account-hash">0x9E39...6D94</div>
+          <div className="actions">
+            <button
+              className={`button alternative small ${
+                isCopied ? "success" : ""
+              }`}
+              onClick={() => {
+                copy("0x9E39...6D94");
+                setIsCopied(true);
+              }}
+            >
+              {isCopied ? "Copied" : "Copy"}
+            </button>
+            <button
+              className="button small"
+              onClick={() => {
+                setIsOpenWalletModal(true);
+              }}
+            >
+              Change
+            </button>
+          </div>
         </div>
-      </div>
-    </DefaultModal>
+      </DefaultModal>
+
+      <WalletModal
+        isOpen={isOpenWalletModal}
+        onClose={() => {
+          setIsOpenWalletModal(false);
+        }}
+      />
+    </>
   );
 };
 
